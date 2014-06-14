a2e6be4c619fd9581463237b1b949082ab358ee4
lawmay <lmay01@gmail.com>
2014-06-13 17:02:46 -0700

    Draw enemies

--- watchout.js
+++ watchout.js
@@ -1,2 +1,72 @@
-// start slingin' some d3 here.
+var gameOptions = {
+  height: 450,
+  width: 700,
+  nEnemies: 30,
+  padding: 20,
+  enemyInterval: 2000
+};
 
+var gameStats = {
+  score: 0,
+  bestScore: 0
+};
+
+var axes = {
+  x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
+  y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
+};
+
+var gameBoard = d3.select('.gameboard')
+                  .append('svg:svg')
+                  .attr('width', gameOptions.width)
+                  .attr('height', gameOptions.height)
+                  ;
+
+var createEnemies = function() {
+  var result = _.range(0,gameOptions.nEnemies);
+  return _.map(result, function(i) {
+    return {
+      id: i,
+      x: Math.random()*100,
+      y: Math.random()*100
+    };
+  });
+};
+
+
+var render = function(enemyData) {
+  console.log("=== entering render ===");
+  var enemies = gameBoard.selectAll('circle.enemy')
+         .data(enemyData, function(d) { return d.id; });
+
+  enemies.enter()
+          .append('svg:circle')
+          .attr('class', 'enemy')
+          .attr('cx', function(enemy) {
+            return axes.x(enemy.x);
+          })
+          .attr('cy', function(enemy) {
+            return axes.y(enemy.y);
+          })
+           .attr('r',10)
+          ;
+
+
+  enemies.exit().remove();
+
+
+};
+
+var play = function() {
+  console.log('===== entering play =====');
+  var gameTurn = function() {
+    console.log('=== executing game turn ===');
+    var newEnemyPositions = createEnemies();
+    render(newEnemyPositions);
+  };
+
+  gameTurn();
+  // setInterval(gameTurn.bind(this), gameOptions.enemyInterval);
+};
+
+play();
